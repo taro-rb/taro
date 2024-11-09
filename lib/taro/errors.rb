b377a5b1a195f94bdc2a4e2f6f44e468515a841f
Janosch Müller <janosch84@gmail.com>
2024-11-09 17:43:46 +0100

    Mark all runtime errors as such

--- lib/taro/errors.rb
+++ lib/taro/errors.rb
@@ -1,5 +1,5 @@
 class Taro::Error < StandardError; end
 class Taro::ArgumentError < Taro::Error; end
 class Taro::RuntimeError < Taro::Error; end
-class Taro::ValidationError < Taro::Error; end
-class Taro::ResponseValidationError < Taro::Error; end
+class Taro::ValidationError < Taro::RuntimeError; end
+class Taro::ResponseValidationError < Taro::RuntimeError; end
