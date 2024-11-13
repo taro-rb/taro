a73f3350fee4f4a67e5174e5e1fad3271524cdfc
Janosch Müller <janosch84@gmail.com>
2024-11-13 19:17:11 +0100

    Add spec

--- spec/taro/rails/dsl_spec.rb
+++ spec/taro/rails/dsl_spec.rb
@@ -1,5 +1,5 @@
 describe Taro::Rails::DSL do
-  let(:controller) { Class.new.extend(described_class) }
+  let(:controller) { stub_const('C', Class.new.extend(described_class)) }
 
   it 'tracks defined_at correctly for an ad-hoc input type' do
     controller.param :foo, type: 'String', null: false
@@ -13,5 +13,18 @@ describe Taro::Rails::DSL do
     declaration = Taro::Rails.buffered_declarations.values.last
     field = declaration.returns[200].fields[:foo]
     expect(field.defined_at.to_s).to match(/#{__FILE__}:\d+/)
+  end
+
+  it 'applies buffered declarations when methods are added',
+     config: { parse_params: false, validate_response: false } do
+    controller.api 'My summary'
+    allow(Taro::Rails::RouteFinder).to receive(:call).and_return([:route])
+    expect { controller.define_method(:foo) { nil } }
+      .to change { Taro::Rails.declarations.count }.by(1)
+  end
+
+  it 'does not fail if actions are defined without buffered declarations' do
+    expect { controller.define_method(:foo) { nil } }
+      .to change { Taro::Rails.declarations.count }.by(0)
   end
 end
